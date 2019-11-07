52aef924453dbc3a3010b0217e38a8e39bde0780
han6054 <424681370@qq.com>
2019-11-07 14:32:58 +0800

    :sparkles: 跨域

---
+++ jsonp/jsonp.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const url = require('url')
+const data = {'data': 'hello'};
+const queryString = require('querystring')
+
+http.createServer((req, res) => {
+    let params = url.parse(req.url);
+    let callback = queryString.parse(params.query).callback
+    if(params.query && callback) {
+      let str = callback + `(${JSON.stringify(data)})`
+        console.log(str);
+        res.end(str)
+    }
+    console.log(queryString.parse(params.query).callback)
+    res.end(JSON.stringify(data))
+
+}).listen(5000,() => {
+    console.log('启动端口号5000')
+})  
