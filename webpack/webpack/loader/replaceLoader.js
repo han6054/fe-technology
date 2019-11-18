751a7585b5aa8197f97c6ba3ff985e70a375ca71
han6054 <424681370@qq.com>
2019-11-18 17:50:45 +0800

    loader

--- webpack/webpack/loader/replaceLoader.js
+++ webpack/webpack/loader/replaceLoader.js
@@ -1,4 +1,14 @@
+const loaderUtils = require('loader-utils')
+
 module.exports = function (source) {
-    const options = this.query;
-    return source.replace('hello', options.name)
+    // const options = this.query;
+    const options = loaderUtils.getOptions(this);
+
+    const callback = this.async()
+    setTimeout(() => {
+        const ret = source.replace('hello', options.name)
+        callback(null, ret)
+    })
+    // return source.replace('hello', options.name)
+
 }
