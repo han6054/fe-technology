02935dc28537e17b0d09c0c5f6e2f733b6bf1e1a
han6054 <424681370@qq.com>
2019-12-05 18:15:23 +0800

    router

--- vue/vue-project/src/main.js
+++ vue/vue-project/src/main.js
@@ -9,4 +9,4 @@ new Vue({
   // store,
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
--- vue/vue-project/src/vue-router.js
+++ vue/vue-project/src/vue-router.js
@@ -1,19 +1,65 @@
 let Vue;
 class Router {
-    constructor () {
-        this.a = 1
+    constructor ({routes, mode}) {
+        this.routeMap = {}; // 路由表
+        this.mode = mode;
+        routes.map(route => {
+            this.routeMap[route.path] = route.component
+        });
+        // 定义响应式数据
+        Vue.util.defineReactive(this, 'route', {current: '/'});
+        // console.log(routeMap);
+        if(mode === 'hash') {
+            window.addEventListener('load', ()=> {
+                location.hash ? '': location.hash = '/'
+            });
+            window.addEventListener('hashchange', ()=> {
+                this.route.current = location.hash.slice(1)
+            });
+        } else {
+            location.pathname?'':location.pathname = '/';
+            window.addEventListener('load',()=>{
+                this.route.current = location.pathname;
+            });
+            window.addEventListener('popstate',()=>{
+                this.route.current = location.pathname;
+            })
+
+        }
     }
 }
-Router.install = (_Vue) => {
-    console.log(_Vue)
+Router.install = (_Vue) => { // Vue中调用use方法，会调用该插件的install方法
     Vue = _Vue;
     Vue.mixin({
-        beforeCreate() {
+        beforeCreate() { // 因为每个组件都有router 在mixin处理
             if(this.$options && this.$options.router) {
                 this._router = this.$options.router
             } else {
-                this._router = this.$parent && this.$parent.router
+                // vue组件的渲染顺序  父 -> 子 -> 孙子 当前组件如果没有传入router向父级查找
+                this._router = this.$parent && this.$parent._router //
             }
+            // 每个组件 $route, $router
+            Object.defineProperty(this, '$route', {
+                value: {}
+            });
+            Object.defineProperty(this, '$router', {
+                value: {}
+            });
+        }
+    });
+    // router-link router-view
+    Vue.component('router-link', {
+        props: {
+            to: String,
+            tag: String
+        },
+        render() {
+            return <a href={this.mode === 'hash'?`#${this.to}`:this.to}>{this.$slots.default}</a>
+        }
+    });
+    Vue.component('router-view', {
+        render(h) {
+            return h(this._router.routeMap[this._router.route.current])
         }
     })
 };
