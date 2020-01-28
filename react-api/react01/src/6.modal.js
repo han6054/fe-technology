c12fe0000ed4994b49bf1a56a5101f32cdf09ccc
han6054 <424681370@qq.com>
2020-01-28 17:56:17 +0800

    插槽

---
+++ react-api/react01/src/6.modal.js
@@ -0,0 +1,39 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom'
+
+
+class Modal extends Component {
+  constructor() {
+      super();
+      this.container = document.querySelector('#modal-root');
+
+  }
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.container)
+
+  }
+}
+
+export default class ModalPage extends Component {
+    constructor() {
+        super();
+        this.state = {show: false}
+    }
+    render() {
+        return(
+            <div>
+                <button onClick={()=> this.setState({show: !this.state.show})}>显示</button>
+                {
+                    this.state ?
+                        <Modal>
+                          <div className="modal-content">
+                              <h1>显示模态窗口</h1>
+                          </div>
+                        </Modal>
+                        : null
+                }
+            </div>
+        )
+    }
+
+}
