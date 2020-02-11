c24b77263b7637ae6b3040a68090444a529f3031
han6054 <424681370@qq.com>
2020-02-11 11:03:04 +0800

    subject

---
+++ test/subject/1.js
@@ -0,0 +1,54 @@
+/*
+1.call 和 apply 区别和性能 ？
+fn.call(obj,1,2,3,4);
+fn.apply(obj,[1,2,3,4]);
+fn函数都是Function原型上的方法，每一个函数作为Function这个原型的实例，可以调取call和apply方法。
+call和apply都是用来改变函数中this指向的，区别在于传参是call一个个传，apply要求以数组形式传给函数，bind并没有把函数立即执行，只是预先处理改变this。
+call比apply性能好一些，传参大约三个的时候。
+*/
+
+// 2. (5).add(3).minus(2);
+!function () {
+    // 每一个方法执行完都要返回Number这个类的实例，这样才可以继续调取Number类中的方法（链式写法）
+    function check(n) {
+        n = Number(n);
+        return isNaN(n) ? 0 : n;
+    }
+    function add(n) {
+        n = check(n);
+       return this + n;
+    }
+    function minus(n) {
+        n = check(n);
+        return this - n;
+    }
+    ['add', 'minus'].forEach((item) => {
+        Number.prototype[item] = eval(item);
+    })
+}();
+console.log((5).add(3).minus(2));
+
+// 3.箭头函数和普通函数的区别？
+
+ // 1.写法简介
+ function fn(x) {
+     return function(y) {
+         return x + y
+     }
+ }
+ let fn = x => y => x + y
+/*
+   2.箭头函数this是所属上下文，是继承函数上下文的this
+*/
+let obj = {};
+function fn1() {
+    console.log(this)
+}
+fn1.call(obj) // this是obj
+let fn2 = () => {
+    console.log(this)
+}
+fn2.call(obj) // this是window
+ // 3.箭头函数没有arguments 类数组
+
+ // 4. 箭头函数不能被new执行，因为箭头函数没有this，也没有prototype
