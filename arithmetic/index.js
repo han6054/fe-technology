6eeb22e77d24a86ffb6d2a676b9317595b5938b3
han6054 <424681370@qq.com>
2021-03-14 19:29:15 +0800

    feat: 算法

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -12,3 +12,41 @@ function findLargestDifference(array) {
     }
     return Math.max(...dValues);
 }
+
+
+// 给定一个正整数n（n>2), 要求输出n的素数分乘；
+
+// 如：输入100， 输出2, 2, 5, 5
+// 如果本身是素数，输出本身，如输入5,输出5
+const result = []
+function split(target) {
+    if(target < 2) return
+    for(let i=2;i<=target;i++) {
+        if(target%i == 0) { 
+            result.push(i); 
+            target = target / i 
+            split(target); 
+            break;
+        }
+    }
+}
+
+const sum = a => b => b?sum(a+b): a
+console.log(sum(1)(2)(3)())
+
+function compose(...func) {
+    if(func.lengt === 1) {
+        return func[0]
+    }
+    return func.reduce((a,b) => (...args) => a(b(...args)))
+}
+function a(str) {
+    return 1+str
+}
+function b(str) {
+    return 2+str
+}
+
+compose(a,b)('haha')
+
+// sleep函数
--- promise/1.js
+++ promise/1.js
@@ -1,17 +1,15 @@
-// 给定一个正整数n（n>2), 要求输出n的素数分乘；
-
-// 如：输入100， 输出2, 2, 5, 5
-// 如果本身是素数，输出本身，如输入5,输出5
-const result = []
-function split(target) {
-    if(target < 2) return
-    for(let i=2;i<=target;i++) {
-        if(target%i == 0) { 
-            result.push(i); 
-            target = target / i 
-            split(target); 
-            break;
-        }
-    }
-}
-
+setTimeout(()=> {
+    console.log('timeout')
+}, 0)
+const p = new Promise((resolve) => {
+    resolve(100)
+    console.log('promise constructor')
+}).then(() => {
+    console.log('promise then 1 ')
+    Promise.resolve().then(() => {
+        console.log('nested promise then')
+    })
+}).then(() => {
+    console.log('promise then 2')
+})
+console.log('normal execution')
